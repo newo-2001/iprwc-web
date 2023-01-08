91f5993ad00279c60d62d5d2521e5d35ea94bc5f
newo-2001 <o.elderbroek@gmail.com>
2023-01-08 15:24:48 +0100

    Moved configuration to .env for ease of deployment

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
     missingThumbnailUrl: "https://www.smartphonekliniek.com/wp-content/uploads/2019/07/no-image.jpg",
-    apiUrl: "http://localhost:8080"
+    apiUrl: process.env["NG_APP_API_URL"]
 }
